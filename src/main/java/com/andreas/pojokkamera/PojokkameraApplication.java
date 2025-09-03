bfbee903eb4117296a02a141aa30d089ea9270de
andrr64 <derzandrr03@gmail.com>
2025-09-03 08:45:43 +0700

    init project

---
+++ src/main/java/com/andreas/pojokkamera/PojokkameraApplication.java
@@ -0,0 +1,13 @@
+package com.andreas.pojokkamera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojokkameraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojokkameraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreas/pojokkamera/PojokkameraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreas.pojokkamera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojokkameraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
